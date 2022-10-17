8e0a24c8a282d84e77ea5f6fd3915269293e094e
SMTeeps <smteeplejr@comcast.net>
2022-10-17 17:14:13 -0400

    board and shelf are displayed

---
+++ constants.py
@@ -0,0 +1,21 @@
+"""
+Collects all the constants needed for the board
+"""
+
+import pygame
+
+# board dimensions
+WIDTH = 400
+HEIGHT = 400
+
+# number of rows and cols on board
+ROWS = 4
+COLS = 4
+
+# individual square size
+SQUARE_SIZE = HEIGHT // ROWS
+
+# colors
+GREEN = (119, 149, 86)
+WHITE = (235, 236, 208)
+BROWN = (100, 46, 28)
---
+++ piece.py
@@ -0,0 +1,14 @@
+from constants import SQUARE_SIZE
+
+class Piece:
+    def __init__(self, number):
+        self.number = number
+        self.x = 0
+        self.y = 0
+        self.calc_pos()
+
+    def calc_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+    def draw(self):
