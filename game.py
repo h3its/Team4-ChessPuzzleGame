832cc3502fe9676f86cc870a22dc6f6a57788e13
Zachary-Maurer <113299007+Zachary-Maurer@users.noreply.github.com>
2022-11-07 17:20:37 -0500

    Zach's json test (#3)
    * whyme
    
    * message
    
    * have to put
    
    * commit
    
    * Fixed board.py in order to avoid the two-screen issue
    
    * added sound to main.py
    
    * Added a timer onto the board
    
    * Removed .idea from git, added basics of loading json game definition
    
    * Removed .idea from git, added basics of loading json game definition, removed pychache from git
    
    * I edited the game to read basic game information from a json file and cut that info out of constants. Also set it up with multiple games with different size game boards and n works to go to the next level
    
    * I made the title reflect the level
    
    Co-authored-by: menjivarj0 <menjivarj0@students.rowan.edu>

---
+++ game.py
@@ -0,0 +1,98 @@
+"""
+Handles game processes including updating the game and moving pieces
+"""
+
+import pygame
+from board import Board
+from constants import *
+
+
+class Game:
+
+    def __init__(self, win, game_definition):
+        self.game_def = game_definition
+        self.ROWS = game_definition['ROWS']
+        self.COLS = game_definition['COLS']
+       # win.
+       # self.SQUARE_SIZE = HEIGHT // ROWS
+        self._init()
+        self.selected = None
+        self.win = win
+
+    """
+    updates board
+    """
+
+    def update(self):
+        self.board.draw(self.win, self.selected)
+        pygame.display.update()
+        if self.board.correct != True:
+            self.board.timer()
+    """
+    sets up initial state of game
+    """
+
+    def _init(self):
+        self.selected = None
+        self.board = Board(self.game_def)
+
+    """
+    resets board to initial state
+    """
+
+    def reset(self):
+        self._init()
+
+    """
+    selects a piece to be moved
+    """
+
+    def pickup(self, row, col):
+        if self.board.wrong:
+            self.board.is_not_wrong()
+        if self.board.get_piece(row, col) != 0:
+            self.selected = self.board.get_piece(row, col)
+    """
+    moves the piece then unselects it
+    """
+
+    def drop(self, row, col):
+        if self.board.get_piece(row, col) == 0:
+            self._move(row, col)
+        self.selected = None
+
+    """
+    moves piece to specified row and col if valid
+    """
+
+    def _move(self, row, col):
+        moveto = self.board.get_piece(row, col)
+        if self.selected and moveto == 0:
+            self.board.move(self.selected, row, col)
+        else:
+            return False
+
+        return True
+
+    """
+    Checks to see if pieces are correctly placed
+    """
+
+    def check_solution(self):
+        result = False
+        if self.board.is_shelf_empty():
+            result = self.board.check()
+        if result:
+            self.board.is_correct()
+        else:
+            self.board.is_wrong()
+            boo_sound.play()
+
+    """
+    Returns True if pieces can be moved, False if not
+    Pieces can be moved until level is completed. Once the solution is checked and "Correct!" is
+    displayed, pieces are not movable.
+    """
+
+    def movable(self):
+        return not self.board.correct
---
+++ main.py
@@ -0,0 +1,116 @@
+"""
+Runs the game and handles user input
+"""
+
+import pygame
+from constants import *
+from game import Game
+import json
+
+FPS = 60
+
+
+"""
+runs the main processes of the game such as drawing the board and shelf
+"""
+
+def load_games_definitions(filename):
+    with open(filename) as file:
+        json_game_definitions = file.read()
+        return json.loads(json_game_definitions)
+
+def get_window_size():
+    d = pygame.display.Info()
+    display_height = d.current_h
+    display_width = d.current_w
+    size = min(display_height, display_width)
+    size = size - size // 2
+    return size,size
+
+game_definition = dict()
+width, height = get_window_size()
+
+def initialize_game(game_num):
+    global game_definition
+    game_definitions = load_games_definitions('gameDefinitions.json')
+    game_definition = game_definitions[game_num]
+    game_definition['WIDTH'], game_definition['HEIGHT'] = width, height
+    game_definition['SQUARE_SIZE'] = game_definition['HEIGHT'] // game_definition['ROWS']
+    game_definition['START_MENU_HEIGHT'] = game_definition['SQUARE_SIZE']
+    game_definition['SHELF_SIZE'] = game_definition['SQUARE_SIZE']
+    ss = game_definition['SQUARE_SIZE']
+    game_definition['QUEEN'] = pygame.transform.scale(pygame.image.load(
+        'assets/queen.png'), (ss, ss))
+    WIN = pygame.display.set_mode(
+        (game_definition['WIDTH'],
+         game_definition['HEIGHT'] + game_definition['SHELF_SIZE'] + game_definition['START_MENU_HEIGHT']))
+    pygame.display.set_caption(game_definition['gameName'])
+    return WIN
+
+
+def main():
+    current_game = 0
+    WIN = initialize_game(current_game)
+
+
+    run = True
+    # normalize game run speed on all hardware
+    clock = pygame.time.Clock()
+
+    game = Game(WIN, game_definition)  # passing in first game definiton into game object
+
+
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if game.movable():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    x, y = pos
+                    if y <= game_definition['HEIGHT'] + game_definition['SHELF_SIZE']:
+                        row, col = get_row_col_from_mouse(pos)
+                        game.pickup(row, col)
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    x, y = pos
+                    if y <= game_definition['HEIGHT'] + game_definition['SHELF_SIZE']:
+                        row, col = get_row_col_from_mouse(pos)
+                        game.drop(row, col)
+                        piece_sound.play()
+
+            if event.type != pygame.KEYDOWN:
+                continue
+
+            if event.key == pygame.K_SPACE:
+                game.check_solution()
+
+            if event.key == pygame.K_r:
+                game.reset()
+
+            if event.key == pygame.K_n:
+                current_game = current_game + 1
+                WIN = initialize_game(current_game)
+                game = Game(WIN, game_definition)
+
+        game.update()
+
+
+"""
+gets the row and col from mouse position
+"""
+
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // game_definition['SQUARE_SIZE']
+    col = x // game_definition['SQUARE_SIZE']
+    return row, col
+
+
+main()
