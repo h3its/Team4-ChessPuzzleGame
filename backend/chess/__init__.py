c146df4de6357626ace7e4891cececcd570fb2a9
Shane Cleary <shanedavidcleary@gmail.com>
2022-12-14 18:27:48 -0500

    memory database working

--- backend/chess/__init__.py
+++ backend/chess/__init__.py
@@ -3,10 +3,12 @@ from flask_httpauth import HTTPBasicAuth
 
 def create_app(env = None):
     app = Flask(__name__, instance_relative_config=False)
+    users = {}
+    scores = {}
 
     auth = HTTPBasicAuth()
     from . import routes
-    routes.configure_routes(app, auth)
+    routes.configure_routes(app, auth, users, scores)
 
     return app
 
--- backend/chess/routes.py
+++ backend/chess/routes.py
@@ -1,11 +1,12 @@
 from flask import abort, request, session
 
-def configure_routes(app, auth):
+def configure_routes(app, auth, users, scores):
 
     @auth.verify_password
     def verify_password(email, password):
-        if email == "test@test.com":
-            return {'email': email, 'password': password}
+        user = users[email]
+        if user and password == user['password']:
+            return user
         else:
             return False
 
@@ -13,6 +14,14 @@ def configure_routes(app, auth):
     def before_request():
         print("REQUEST...")
         print(request.method, request.endpoint, request.authorization)
+
+    @app.route("/users", methods=['POST'])
+    def create_user():
+        email = request.json.get('email')
+        password = request.json.get('password')
+        user = { 'email': email, 'password': password }
+        users[email] = user
+        return user
 
     @app.route("/users", methods=['GET'])
     def get_users():
@@ -23,5 +32,33 @@ def configure_routes(app, auth):
     def get_user():
         user = auth.current_user()
         return user['email']
+    
+    @app.route("/scores", methods=['POST'])
+    @auth.login_required
+    def save_scores():
+        score = request.json.get('score')
+        level = request.json.get('level')
+        user = auth.current_user()
+        email = user['email']
+        score_entry = { 'score': score, 'level': level }
+        user_scores = scores.get(email)
+        if user_scores: 
+            user_scores.append(score_entry)
+        else:
+            scores[email] = [score_entry]
         
+        return score_entry
+
+    @app.route("/scores/top", methods=['GET'])
+    @auth.login_required
+    def get_high_score():
+        user = auth.current_user()
+        email = user['email']
+        user_scores = scores.get(email)
+        if user_scores:
+            slist = sorted(user_scores, key=lambda se: se['score'], reverse=True)
+            return slist[0]
+        else:
+            return None
+
 
